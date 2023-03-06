dc2034b911191b91ccfab3164b51e75297d8a951
roadblock37 <Roadblock37@gmail.com>
2023-03-06 16:02:54 -0500

    code cleanup

--- server.js
+++ server.js
@@ -1,42 +1,37 @@
-const express = require('express');
-require('dotenv').config();
-require('express-async-errors');
-const cors = require('cors');
-const path = require('path');
-const connectDB = require('./DB/connect');
-const invoiceRouter = require('./Routers/invoiceRouter')
+const express = require("express");
+require("dotenv").config();
+require("express-async-errors");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./DB/connect");
+const invoiceRouter = require("./Routers/invoiceRouter");
 
 const app = express();
-
-
-
 
 // middleware
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(express.json());
 
-// routers
-app.use('/api/v1/invoices', invoiceRouter);
-
 // routes
+app.use("/api/v1/invoices", invoiceRouter);
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./public", "index.html"));
-  });
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
 
-  // port variable
+// port variable
 const port = process.env.PORT || 5000;
 
 // server start function
-const start = async() => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, () => {
-            console.log(`Server is listening on port: ${port}`);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start();
