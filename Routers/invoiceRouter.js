41074fd9221c6c0b985b31648d49d612c0a2537f
Paul Biber <roadblock37@gmail.com>
2023-03-13 16:11:48 -0400

    working on urls for data fetching

--- Routers/invoiceRouter.js
+++ Routers/invoiceRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {getAllInvoices,getSingleInvoice,createInvoice,updateInvoice,deleteInvoice} = require('../Controllers/invoiceController');
 
-router.route('/').get(getAllInvoices).post(createInvoice);
+router.route('/invoices').get(getAllInvoices).post(createInvoice);
 router.route('/:id').get(getSingleInvoice).delete(deleteInvoice).patch(updateInvoice);
 
 module.exports = router;
